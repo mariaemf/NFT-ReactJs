a002e1a2cc24a1d873e2d4d9c69aeb225cc8c474
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-16 18:33:04 -0300

    creation and routing of page pages

--- src/assets/routes/Routers.js
+++ src/assets/routes/Routers.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Contact from "../pages/Contact";
+import Create from "../pages/Create";
+import EditProfile from "../pages/EditProfile";
+import Home from "../pages/Home";
+import Market from "../pages/Market";
+import NftDetails from "../pages/NftDetails";
+import SellerProfile from "../pages/SellerProfile";
+import Wallet from "../pages/Wallet";
+
 const Routers = () => {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/market" element={<Market />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/seller-profile" element={<SellerProfile />} />
+      <Route path="/edit-profile" element={<EditProfile />} />
+      <Route path="/market/:id" element={<NftDetails />} />
+      <Route path="/wallet" element={<Wallet />} />
+    </Routes>
+  );
 };
 
 export default Routers;
