de18f360afc2a10904a946f262aad744f76bd5cc
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-19 20:39:45 -0300

    performing HeroSection assigning images and stylizations directly to the css of it

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
+import heroImg from "../../assets/images/hero.jpg";
 import "./hero-section.css";
 
 const HeroSection = () => {
@@ -12,7 +13,7 @@ const HeroSection = () => {
             <div className="hero__content">
               <h2>
                 Discover rare digital art and collect
-                <span>sell extraordinary</span>NFTs
+                <span>sell extraordinary</span> NFTs
               </h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -21,15 +22,21 @@ const HeroSection = () => {
               </p>
 
               <div className="hero__btns d-flex aling-items-center gap-4">
-                <button className="d-flex aling-items-center gap-2">
+                <button className="explore__btn d-flex aling-items-center gap-2">
                   <i class="ri-rocket-line"></i>
                   <Link to="/market">Explore</Link>
                 </button>
-                <button className="d-flex aling-items-center gap-2">
+                <button className="create__btn d-flex aling-items-center gap-2">
                   <i class="ri-ball-pen-line"></i>
-                  <Link to="/create">Explore</Link>
+                  <Link to="/create">Create</Link>
                 </button>
               </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="hero__img">
+              <img src={heroImg} alt="" className="w-100" />
             </div>
           </Col>
         </Row>
