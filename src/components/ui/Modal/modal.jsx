7dfdb7cd35719fd23dadf403dcd690c25c0cdf97
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-23 18:45:19 -0300

    creating modal and its stylizations, also adding changes directly in NFT-CARD for better modal utilization

--- src/components/ui/Modal/modal.jsx
+++ src/components/ui/Modal/modal.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import "./modal.css";
 
-const modal = () => {
+const modal = ({ setShowModal }) => {
   return (
-    <div className="modal__wrapper">
-      <div className="modal">
-        <h6>Place a Bid</h6>
-        <p>
-          You must bid at least <span>5.89 ETH</span>
+    <div className="modal__wrapper" onClick={() => setShowModal(false)}>
+      <div className="single__modal">
+        <h6 className="text-center text-light">Place a Bid</h6>
+        <p className="text-center text-light">
+          You must bid at least <span className="money">5.89 ETH</span>
         </p>
+
+        <div className="input__item">
+          <input type="number" placeholder="00 : 00 ETH" />
+        </div>
+
+        <div className="input__item">
+          <h6>Enter Quantity, 7 avaliable</h6>
+          <input type="number" placeholder="00 : 00 ETH" />
+        </div>
+        <p className="d-flex aling-items-center justify-content-between">
+          You must bid at least <span className="money">5.89 ETH</span>
+        </p>
+
+        <div className="d-flex aling-items-center justify-content-between">
+          <p>Service Fee</p>
+          <span className="money ">0.89 ETH</span>
+        </div>
+
+        <div className="d-flex aling-items-center justify-content-between">
+          <p>Total Bid Amount</p>
+          <span className="money">5.89 ETH</span>
+        </div>
+
+        <button className="place__bid-btn">place a Bid</button>
       </div>
     </div>
   );
--- src/components/ui/Nft-card/NftCard.jsx
+++ src/components/ui/Nft-card/NftCard.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {
+  useState,
+} from "react"; /*o useState permite a criação de estado no componente através da função e faz o gerenciamento do estado local do componente retornando uma array com o resultado*/
 import { Link } from "react-router-dom";
+import Modal from "../Modal/modal";
 import "./nft-card.css";
 
 const NftCard = (props) => {
   const { title, id, currentBid, creatorImg, imgUrl, creator } = props.item;
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className="single__nft__card">
@@ -35,9 +40,15 @@ const NftCard = (props) => {
         </div>
 
         <div className="mt-3 d-flex align-items-center justify-content-between">
-          <button className="bid__btn d-flex align-items-center gap-1">
+          <button
+            className="bid__btn d-flex align-items-center gap-1"
+            onClick={() => setShowModal(true)}
+          >
             <i class="ri-shopping-bag-line"></i>Place Bid
           </button>
+
+          {showModal && <Modal setShowModal={setShowModal} />}
+
           <span className="history__link">
             <Link to="#">View History</Link>
           </span>
