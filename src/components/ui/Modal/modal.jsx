1dac0dea38cb41d77fda9832a1c429cfac2e47da
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-23 20:01:46 -0300

    responsive modal completion and styling

--- src/components/ui/Modal/modal.jsx
+++ src/components/ui/Modal/modal.jsx
@@ -3,27 +3,30 @@ import "./modal.css";
 
 const modal = ({ setShowModal }) => {
   return (
-    <div className="modal__wrapper" onClick={() => setShowModal(false)}>
+    <div className="modal__wrapper">
       <div className="single__modal">
+        <span className="close__modal">
+          <i class="ri-close-line" onClick={() => setShowModal(false)}></i>
+        </span>
         <h6 className="text-center text-light">Place a Bid</h6>
         <p className="text-center text-light">
           You must bid at least <span className="money">5.89 ETH</span>
         </p>
 
-        <div className="input__item">
+        <div className="input__item mb-4">
           <input type="number" placeholder="00 : 00 ETH" />
         </div>
 
-        <div className="input__item">
+        <div className="input__item mb-3">
           <h6>Enter Quantity, 7 avaliable</h6>
-          <input type="number" placeholder="00 : 00 ETH" />
+          <input type="number" placeholder="Enter quantity" />
         </div>
         <p className="d-flex aling-items-center justify-content-between">
           You must bid at least <span className="money">5.89 ETH</span>
         </p>
 
         <div className="d-flex aling-items-center justify-content-between">
-          <p>Service Fee</p>
+          <p>Service Free</p>
           <span className="money ">0.89 ETH</span>
         </div>
 
