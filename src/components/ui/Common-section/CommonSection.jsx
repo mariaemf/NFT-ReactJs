7e1ba86563a7370c0b81298c99bcf8b44958afde
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-24 15:38:19 -0300

    making a new common-section route and adding its stylizations and also a options section

--- src/components/ui/Common-section/CommonSection.jsx
+++ src/components/ui/Common-section/CommonSection.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "reactstrap";
-import "./cpmmon-section.css";
+import "./common-section.css";
 
 const CommonSection = ({ title }) => {
   return (
     <section className="common__section">
-      <Container className="text-center">{title}</Container>
+      <Container className="text-center">
+        <h2>{title}</h2>
+      </Container>
     </section>
   );
 };
--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import CommonSection from "../components/ui/Common-section/CommonSection";
 const Market = () => {
   return (
-    <div>Market</div>
-  )
-}
+    <>
+      <CommonSection title={"MarketPlace"} />
 
-export default Market
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12"></Col>
+            <div className="market__product__filter">
+              <div className="filter__left">
+                <div className="all_categorry__filter">
+                  <h6>All Categories</h6>
+                  <select>
+                    <option value="art">Art</option>
+                    <option value="music">Music</option>
+                    <option value="domain-name">Domain Name</option>
+                    <option value="virtual-word"> Virtual Name</option>
+                    <option value="trending-card">Trending Cards</option>
+                  </select>
+                </div>
+              </div>
+              <div className="filter__right"></div>
+            </div>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Market;
