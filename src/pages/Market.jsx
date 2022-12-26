36d096807238745fcff45adab2c14cf34fd0c7b5
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-26 20:23:51 -0300

    adding methods and variables to the Market page

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Col, Container, Row } from "reactstrap";
+import { NFT__DATA } from "../assets/data/data";
 import CommonSection from "../components/ui/Common-section/CommonSection";
+import NftCard from "../components/ui/Nft-card/NftCard";
+
 const Market = () => {
+  const handleCategory = () => {};
+
+  const handleItems = () => {};
+
+  const handleSort = () => {};
+
   return (
     <>
       <CommonSection title={"MarketPlace"} />
@@ -9,22 +18,44 @@ const Market = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="12"></Col>
-            <div className="market__product__filter">
-              <div className="filter__left">
-                <div className="all_categorry__filter">
-                  <h6>All Categories</h6>
-                  <select>
-                    <option value="art">Art</option>
-                    <option value="music">Music</option>
-                    <option value="domain-name">Domain Name</option>
-                    <option value="virtual-word"> Virtual Name</option>
-                    <option value="trending-card">Trending Cards</option>
+            <Col lg="12" className="mb-5">
+              <div className="market__product__filter d-flex aling-items-center justify-content-between ">
+                <div className="filter__left d-flex aling-items-center gap-5">
+                  <div className="all_categorry__filter">
+                    <select onChange={handleCategory}>
+                      <option>All Categories</option>
+                      <option value="art">Art</option>
+                      <option value="music">Music</option>
+                      <option value="domain-name">Domain Name</option>
+                      <option value="virtual-word"> Virtual Name</option>
+                      <option value="trending-card">Trending Cards</option>
+                    </select>
+                  </div>
+
+                  <div className="all_items__filter">
+                    <select onChange={handleItems}>
+                      <option>All Items</option>
+                      <option value="single-items">Single Item</option>
+                      <option value="bundle">Bundle</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="filter__right">
+                  <select onChange={handleSort}>
+                    <option>Sort By</option>
+                    <option value="hight">Hight Rate</option>
+                    <option value="mid">Mid Rate</option>
+                    <option value="low">Low Rate</option>
                   </select>
                 </div>
               </div>
-              <div className="filter__right"></div>
-            </div>
+            </Col>
+            {NFT__DATA.map((item) => (
+              <Col lg="3" md="4" sm="6" className="mb-4" key={item.id}>
+                <NftCard item={item} />
+              </Col>
+            ))}
           </Row>
         </Container>
       </section>
